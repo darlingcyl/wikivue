f0fc89e9f3b49c465ff97afe25fc2b8f2ba77a34
yinguiwang <2529090407@qq.com>
2023-04-21 22:36:33 +0800

    第一次提交，创建了springboot项目，增加了.gitignore文件，配置了pom.xml文件

---
+++ src/main/java/com/frank/wiki/App.java
@@ -0,0 +1,26 @@
+package com.frank.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@SpringBootApplication
+@RestController
+public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+//    @value("${driverName:man}")
+//   String name;
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
